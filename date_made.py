6d3d0251406d3c8ba3dce8dc00245147b055c171
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2018-01-19 14:52:08 -0800

    Fix some logic throughout.

--- date_made.py
+++ date_made.py
@@ -43,11 +43,12 @@ def get_date_alpha(url_fun,month_dict):
                 if(before.isdigit() and (len(before) == 1 or len(before) == 2 or len(before) == 4)):
 
                     if(after.isdigit() and (len(after) == 1 or len(after) == 2 or len(after) == 4)):
-                        print before, word, after
+                        print before, word, after,
                         date_list.append((str(before),str(word),str(after)))
 
                 #i.e: Jan 4 2017
                 elif(after.isdigit() and (len(after) ==1 or len(after) == 2 or len(after) == 4)):
+
                     possible_year = site[word_index+2]
                     if(possible_year.isdigit()):
                         #print word,after,possible_year
@@ -96,16 +97,18 @@ def reformat_date(dates):
 
 #could do some checks based on dates...
 def date_finder(url_fun,month_dict):
+
+    #two modes for input: html page and pdf
+    #pdf...
     dates1 = get_date_alpha(url_fun,month_dict)
     dates2 = get_date_num(url_fun)
-    dates = reformat_date(dates2)
+    dates2 = reformat_date(dates2)
     # will be the wayback machine here at some point!
     # the wayback machine will be able to verify if the date is correct.
-    dates2 = [("1","2","2017"),("1","2","2017"),("1","2","2017")]
-    dates1,dates2 = combine_dates(dates1,dates2)
-
+    #dates2 = [("1","2","2017"),("1","2","2017"),("1","2","2017")]
+    dates1,dates2 = fix_dates(dates1,dates2)
     if(len(dates1) < 2):
-        counter = len(dates)
+        counter = len(dates1)
     else:
         counter = 2
 
@@ -120,10 +123,11 @@ def date_finder(url_fun,month_dict):
         counter = 2
     for i in range(counter):
         print dates2[i]
-    
+
     print "Go check the page for these dates! One of these is likely!"
-#Combines the two rows of dates
-def combine_dates(stack1,stack2):
+
+#Returns two lists, each has completely legal dates.
+def fix_dates(stack1,stack2):
     dates1 = list()
     dates2 = list()
     for date in stack1:
@@ -143,9 +147,9 @@ def is_valid_date(date,format_type):
     """
 
     #for the values with strings
-    spot1 = date[0]
-    spot2 = date[1]
-    spot3 = date[2]
+    spot1 = date[0] #day/month
+    spot2 = date[1] #day/month
+    spot3 = date[2] #year
 
     #string dates
     if(format_type == 1):
@@ -194,22 +198,18 @@ def test_urls(month_dict):
 
     for url in url_lst:
         dates = get_date_alpha(url,month_dict)
-        print find_date(dates)
+        #print find_date(dates)
         assert(len(dates) > 0)
     print("Finish Tests!")
 
 #Wil have a working wayback machine API in order to find the first date that this
 #website page appeared on the internet.
 def wayback_view(url_fun):
-    #url = "https://web.archive.org/web/*/" + url_fun
-    #source = str(url.encode('UTF-8'))
-    #r = requests.get(source)
-    #site = "t t t Nov 5 '08"
-    #site = str(r.text.encode('UTF-8'))
-    #site = cleanhtml(site)
     pass
 
 def main():
+
+    #need to remake this dictionary.
     month_dict = dict()
     month_dict["January"] = ("Jan",31,1)
     month_dict["February"] = ("Feb",29,2)
@@ -220,6 +220,7 @@ def main():
     month_dict["July"] = ("Jul",31,7)
     month_dict["August"] = ("Aug",31,8)
     month_dict["September"] = ("Sept",30,9)
+    month_dict["Sep"] = ("Sep",30,13)
     month_dict["October"] = ("Oct",31,10)
     month_dict["November"] = ("Nov",30,11)
     month_dict["December"] = ("Dec",31,12)
@@ -228,10 +229,10 @@ def main():
     #print(is_valid_date(['May','1','00'],1))
     #print(get_date_num('http://www.vh1.com/shows/americas-next-top-model'))
     #test_urls(month_dict)
-    #url = str(raw_input("Give me an url that I should check: "))
-    url = "https://stackoverflow.com/questions/16908186/python-check-if-list-items-are-numbers"
+    #test_urls(month_dict)
+    url = str(raw_input("Give me an url that I should check: "))
+    #url = "https://www.youtube.com/watch?v=r33bH3Jioq8"
     date_finder(url,month_dict)
-
 
 
 main()
